986e0879cdce9f70c80493e1981c6fe1f69cd3ea
Jordan Whitlock <jnwhitlock1@gmail.com>
2022-01-31 20:44:47 -0600

    Pseudo-code for project

---
+++ index.js
@@ -0,0 +1,89 @@
+// Function computerPlay returns random choice of rock, paper, or scissors, stores it in variable computerSelection
+
+// Function changeCase takes user input and returns version with first letter capitalized and rest not
+
+// Function game {
+
+    // Function match {
+
+        // User inputs choice, stored in variable playerSelection
+
+        // Run computerPlay
+
+        // If user inputs rock
+
+            // Check if computer chose rock
+
+                // If it did, result is tie
+
+            // Check if computer chose paper
+
+                // If it did, result is computer wins
+
+            // Check if computer chose scissors
+
+                // If it did, result is user wins
+
+        // If user inputs paper
+
+            // Check if computer chose rock
+
+                // If it did, result is user wins
+
+            // Check if computer chose paper
+
+                // If it did, result is tie
+
+            // Check if computer chose scissors
+
+                // If it did, result is computer wins
+
+        // If user inputs scissors
+
+            // Check if computer chose rock
+
+                // If it did, result is computer wins
+
+            // Check if computer chose paper
+
+                // If it did, result is user wins
+
+            // Check if computer chose scissors
+
+                // If it did, result is computer wins
+
+        // If result is tie 
+
+            // Return "You tied! Try again!"
+
+        // If result is user wins
+
+            // Return "You win! X beats Y"
+
+        // If result is computer wins
+
+            // Return "Computer wins! X beats Y"
+
+        // Otherwise, give an error message "Oops, something went wrong"
+
+    // }
+
+    // Run Match
+
+    // Run Match
+
+    // Run Match
+
+    // Run Match
+
+    // Run Match
+
+    // If user won 3+ matches,
+
+        // Return "You won! Congratulations!"
+
+    // If computer won 3+ matches
+
+        // Return "Oof, you lost! That sucks!"
+
+// }
