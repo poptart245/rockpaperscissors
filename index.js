a4b43bb91b25ce5f02496e4d62e58938e9c58080
Jordan Whitlock <jnwhitlock1@gmail.com>
2022-02-02 16:20:52 -0600

    Finished console version of project!

--- index.js
+++ index.js
@@ -2,26 +2,38 @@ function capitalize(playerPrompt) {
     return playerPrompt.charAt(0).toUpperCase() + playerPrompt.slice(1).toLowerCase();
 }
 
+let userWins;
+let compWins;
+
+// determines winner from player and computer choices
 function playRound(playerSelection, computerSelection) {
-    let result;
-    if (playerSelection === "Rock") {
-        (computerSelection === "Rock") ? result = "You tied!" :
-        (computerSelection === "Paper") ? result = "You lose! Paper beats Rock!" :  result = "You win! Rock beats Scissors!";
-    } else if (playerSelection === "Paper") {
-        (computerSelection === "Rock") ? result = "You win! Paper beats Rock!" :
-        (computerSelection === "Paper") ? result = "You tied!" : result = "You lose! Scissors beats paper!";
-    } else if (playerSelection === "Scissors") {
-        (computerSelection === "Rock") ? result = "You lose! Rock beats Scissors!" :
-        (computerSelection === "Paper") ? result = "You win! Scissors beats Paper!" : result = "You tied!";
+    if ((playerSelection === "Rock") && (computerSelection === "Scissors") || 
+        (playerSelection === "Paper") && (computerSelection === "Rock") || 
+        (playerSelection === "Scissors") && (computerSelection === "Paper")) {
+            result = "You win! " + playerSelection + " beats " + computerSelection + ".";
+            userWins++;
+    } else if ((playerSelection === "Rock") && (computerSelection === "Paper") || 
+        (playerSelection === "Paper") && (computerSelection === "Scissors") || 
+        (playerSelection === "Scissors") && (computerSelection === "Rock")) {
+            result = "You lose! " + computerSelection + " beats " + playerSelection + ".";
+            compWins++;
+    } else if ((playerSelection === "Rock") && (computerSelection === "Rock") || 
+        (playerSelection === "Paper") && (computerSelection === "Paper") || 
+        (playerSelection === "Scissors") && (computerSelection === "Scissors")) {
+            result = "You tied!";
     } else {
-        result =  "Oops, something went wrong.";
+        result =  "error";
     }
+    
     console.log(result);
+    return result;
 }
+
+// actually runs game
 function game() {
     let playerPrompt = prompt("You choose: Rock, Paper, or Scissors?");
     let playerSelection = capitalize(playerPrompt);
-
+    
     const computerChoices = [
         "Rock",
         "Paper",
@@ -29,11 +41,39 @@ function game() {
     ]
     let computerSelection = computerChoices[Math.floor(Math.random() * 
     computerChoices.length)];
-    console.log(computerSelection);
+
     playRound(playerSelection, computerSelection);
 }
-game();
-game();
-game();
-game();
-game();
+
+// runs 5 matches of game
+function wholeGame() {
+    userWins = 0;
+    compWins = 0;
+
+    game();
+    console.log("You: " + userWins + " Computer: " + compWins);
+
+    game();
+    console.log("You: " + userWins + " Computer: " + compWins);
+
+    game();
+    console.log("You: " + userWins + " Computer: " + compWins);
+
+    game();
+    console.log("You: " + userWins + " Computer: " + compWins);
+
+    game();
+    console.log("You: " + userWins + " Computer: " + compWins);
+
+    if (compWins > userWins) {
+        console.log("Awww you lost! Try again!");
+    } else if (userWins > compWins) {
+        console.log("Sweet! You won!!! Congratulations!");
+    } else if (userWins === compWins) {
+        console.log("Uh oh, you tied!")
+    } else {
+        console.log("Something went wrong...");
+    }
+}
+
+wholeGame();
